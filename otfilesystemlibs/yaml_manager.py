cce248b17a0fdbd771eb0734ce3b867bcaa30b28
Rajat vats <rajat.vats@opstree.com>
2022-01-30 12:33:47 +0530

    WIP

--- otfilesystemlibs/yaml_manager.py
+++ otfilesystemlibs/yaml_manager.py
@@ -6,12 +6,12 @@ class getYamlLoader:
     def _loadYaml(self,yaml_file_path):
         
         try: 
-            load_file = open(self.yaml_file_path)
+            load_file = open(yaml_file_path)
             parse_yaml = yaml.load(load_file, Loader=yaml.FullLoader)
             logging.info(f"Loaded conf file succesfully.")
             return parse_yaml
         except FileNotFoundError:
-            logging.error(f"Unable to find conf file < {self.yaml_file_path} > Please mention correct property file path.")
+            logging.error(f"Unable to find conf file < {yaml_file_path} > Please mention correct property file path.")
             exit()
 
         return None
