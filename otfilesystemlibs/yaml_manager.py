894b03b27a5cab76a8e8c9b172d09ece8a6671e7
Rajat vats <rajat.vats@opstree.com>
2022-01-30 11:58:16 +0530

    * Added yaml loader lib * Made lib as pip installable

---
+++ otfilesystemlibs/yaml_manager.py
@@ -0,0 +1,17 @@
+import logging
+import yaml
+
+class getYamlLoader:
+        
+    def _loadYaml(self,yaml_file_path):
+        
+        try: 
+            load_file = open(self.yaml_file_path)
+            parse_yaml = yaml.load(load_file, Loader=yaml.FullLoader)
+            logging.info(f"Loaded conf file succesfully.")
+            return parse_yaml
+        except FileNotFoundError:
+            logging.error(f"Unable to find conf file < {self.yaml_file_path} > Please mention correct property file path.")
+            exit()
+
+        return None
